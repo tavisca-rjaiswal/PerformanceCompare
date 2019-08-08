405d72923a9b37ce1f65e5110ea8d266b9fa1021
Jaiswal <rjaiswal@internal.tavisca.com>
2019-08-08 12:09:59 +0530

    First Commit

---
+++ PerformanceCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
